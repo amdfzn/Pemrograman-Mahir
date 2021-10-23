9e029f6079bdca96536f6eb4ef4b0bf338871336
Ahmad Fauzan <12050111663@students.uin-suska.ac.id>
2021-10-23 14:29:41 +0700

    Tugas 2 ArrayStatis

---
+++ src/Tugas2/Array/ArrayStatis.java
@@ -0,0 +1,10 @@
+package Tugas2.Array;
+
+public class ArrayStatis {
+    public static void main(String[] args) {
+        int[] angka = {2, 3, 4, 5, 6};
+        for (int i = 0; i < angka.length; i++){
+            System.out.print(angka[i]);
+        }
+    }
+}
